a1ce20787b7914e98808573bc2ee0c17ff74b72f
Ghasem Elyasi <ghasem.elyasi@swisscom.com>
2021-03-26 22:58:47 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Widgets - Miro
+
---
+++ src/test/java/com/miro/assignments/widgets/WidgetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.assignments.widgets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
